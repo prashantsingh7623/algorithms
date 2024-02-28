335299068cef987428b0d9aa89517da48ea2e4cd
prashant singh <prashantsingh@prashants-MacBook-Air.local>
2024-02-28 23:22:23 +0530

    added modified binary search using loop

--- recursion/binary_search.py
+++ recursion/binary_search.py
@@ -51,7 +51,26 @@ def modified_binary_search(nums: list, target: int, low: int, high: int) -> bool
         high = mid-1
         return modified_binary_search(nums=nums, target=target, low=low, high=high)
       
+def modified_binary_search_using_loop(arr: list, ele:int) -> bool:
+  low = 0
+  high = len(arr)-1
+  while low <= high:
+    mid = (low + high)//2
+    if arr[mid] == ele:
+      return True
+    if arr[mid] > arr[low]:
+      if ele >= arr[low] and ele < arr[mid]:
+        high = mid - 1
+      else:
+        low = mid + 1
+    else:
+      if ele > arr[mid] and ele <= arr[high]:
+        low = mid + 1
+      else:
+        high = mid - 1
+  return False
 
 # print(binary_search(nums=arr,target=200,low=0,high=len(arr)-1))
 # print(binary_search_using_loop(arr=arr, ele=100))
-print(modified_binary_search(nums=arr_rotated, target=9, low=0, high=len(arr_rotated)-1))
+# print(modified_binary_search(nums=arr_rotated, target=9, low=0, high=len(arr_rotated)-1))
+print(modified_binary_search_using_loop(arr=arr_rotated, ele=9))
